47cc20543ec5a02e650b2ea6f4585805aca6c13a
alex-m-perez <alex.murciano.p@gmail.com>
2025-06-17 20:10:51 +0200

    fix conflict

--- src/main/java/es/laboticademar/webstore/repositories/FacturaDAO.java
+++ src/main/java/es/laboticademar/webstore/repositories/FacturaDAO.java
@@ -2,14 +2,7 @@ package es.laboticademar.webstore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<<< HEAD:src/main/java/es/laboticademar/webstore/repositories/FacturaDAO.java
 import es.laboticademar.webstore.entities.Factura;
 
 public interface FacturaDAO extends JpaRepository<Factura, Long> {
-========
-import es.laboticademar.webstore.entities.Familia;
-
-public interface FamiliaRDAO extends JpaRepository<Familia, Long> {
->>>>>>>> main:src/main/java/es/laboticademar/webstore/repositories/FamiliaRDAO.java
-    // No es necesario definir un método findAll() aquí
 }
